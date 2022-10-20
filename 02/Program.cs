2e95b0209852540bd8969547b08a2ca6ae566e49
ys-off <yuriy.safronoff@yandex.ru>
2022-10-20 17:29:17 +0300

    02

---
+++ 02/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число C");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine($"max:{max}");
